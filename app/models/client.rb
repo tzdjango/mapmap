15f1e463d26e1805714c741ca9d7b10855c408a0
taza99 <tayyab.zaheer@yahoo.com>
2016-09-15 13:26:06 +0000

    Client table and its direct associations, some modifications to CLient class

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ActiveRecord::Base
   has_many :clientcontactinfo, dependent: :destroy
   has_many :clientaddress, dependent: :destroy
   has_many :userauth, dependent: :destroy
+  has_many :ownershiphistory, dependent: :destroy
+  has_many :messagesmeta, dependent: :destroy
 end
